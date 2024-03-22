cf9fc33b4096f9dfc5ee40d5e2bda71ce2b6d9aa
tobias-nolz <tobias.nolz@gmx.at>
2024-03-22 11:47:55 +0100

    added cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT | 3000
 const host = process.env.HOST | '0.0.0.0'
+
+app.use(cors())
 
 app.get('/*', (req, res) => {
     console.log({
